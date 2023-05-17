1c9fe0e284761182b55990be31bb8a548ec9c449
didasoff <dmd51515@gmail.com>
2023-05-17 06:38:00 +0600

    adding in sanity studio

---
+++ hera-dev/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '5a2z2ifw',
+    dataset: 'hera-dev'
+  }
+})
---
+++ src/client.js
@@ -0,0 +1,6 @@
+import sanityClient from '@sanity/client'
+
+export default sanityClient({
+      projectId: "5a2z2ifw",
+      dataset: "hera-dev"
+})
